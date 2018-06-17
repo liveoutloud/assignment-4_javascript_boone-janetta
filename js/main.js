40f36e4f16639dd278bd3e09e087c30a80ee1134
Janetta Boone <j.boone@ufl.edu>
2018-06-17 17:39:18 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/***** Primitive Types *****/
+
+// strings
+var string1 = "I am a string";
+var string2 = 'I am another string';
+
+// numbers
+var num = 0;
+var num1 = 1;
+var num2 = 45;
+var num3 = -32;
+var num4 = 3.14;
+
+var num5 = '1000'; //caution -- this is actually a string!
+
+// boolean
+var bool1 = true;
+var bool2 = false;
+
+// null
+var nullVar = null;
+
+// undefined
+var undefinedVar1 = undefined;
+var undefinedVar2; //undefined by default.  lines 24 and 25 are the exact same!
+
+/***** Complex Types or Object Types *****/
+// objects
+var myObject = {
+  key: 'value' //the value can be any of the primitives
+}
+
+// arrays
+var myArray = [1, 2, 3];
+
+//functions
+var myFunction = function() {
+  console.log( 'I am a function!' );
+}
