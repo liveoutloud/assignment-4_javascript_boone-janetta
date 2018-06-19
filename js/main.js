d833e2b77805a08e8e49a03eb6ae1dc2774e765a
Janetta Boone <j.boone@ufl.edu>
2018-06-18 20:55:51 -0500

    added functions

--- js/main.js
+++ js/main.js
@@ -1,110 +1,12 @@
-/***** Primitive Types *****/
-
-/*// strings
-var string1 = "I am a string";
-var string2 = 'I am another string';
-
-// numbers
-var num = 0;
-var num1 = 1;
-var num2 = 45;
-var num3 = -32;
-var num4 = 3.14;
-
-var num5 = '1000'; //caution -- this is actually a string!
-
-// boolean
-var bool1 = true;
-var bool2 = false;
-
-// null
-var nullVar = null;
-
-// undefined
-var undefinedVar1 = undefined;
-var undefinedVar2; //undefined by default.  lines 24 and 25 are the exact same!
-
-/***** Complex Types or Object Types *****/
-// arrays
-/*var myArray = [1, 2, 3];
-
-var shoppingList = ['milk', 'ice cream', 'flour'];
-//                    0         1           2
-
-var shoppingList2 = [
-  'milk',
-  'ice cream',
-  'flour'
-];
-
-var listOfThings = [
-  'strings are an acceptable type',
-  function mySpecialFunction() {
-    console.log('hello ');
-  },
-  365,
-  {
-    key: 'Richard'
-  },
-  true,
-  [1, 'two', 3333333],
-  null,
-  undefined,
-];
-
-// objects
-var myObject = {
-  key: 'value' //the value can be any of the primitives
-}
-
-var family = {
-  dad: 'Rick',
-  mom: 'Barbara',
-  age: 42,
-  isJavaScriptAwesome: true,
-  brother: 'Jonathan',
-  nephew: 'Jack'
-};
-
-var AdvWeb2 = {
-  instructor: {
-    fullName: "Richard Kalehoff"
-  },
-  name: "Advanced Web 2",
-  numberOfStudents: 4,
-  students: [
-    {name: "Allison", ufID: 112233, grades: [2, 1, 3]},
-    {name: "Jack", ufID: 445566, grades: [5, 4, 6]},
-    {name: "James", ufID: 778899, grades: [8, 7, 9]},
-    {name: "Barbara", ufID: 000000, grades: [1, 1, 94]},
-  ],
-  toString: function() {
-    return AdvWeb2.numberOfStudents;
-  }
-};
-
-//functions
-var myFunction = function() {
-  console.log( 'I am a function!' );
-}
-
-// identifiers
-var ABCabc = 'hello';
-var abcABC = 'there';
-var _howdy = 123;
-var $testing = [1, 2, 3];*/
-
-/**********End of Notes with Richard**********/
-
 /**********Start of Assignment 4**********/
 
 //step 4
 var iceCream = ['Mint Chocolate Chip', 'Pistachio', 'Strawberry'];
 
 iceCream.push('Cookie Dough');
+iceCream.length === 4;
 
 //step 5
-
 var presidents;
 presidents = [
   presidentInfo = {
@@ -151,3 +53,38 @@ presidents = [
 var presidents = [presidentInfo, presidentInfo1, presidentInfo2, presidentInfo3, presidentInfo4];
 
 console.log(presidents[2]. firstName + '' + presidents[2]. lastName);
+
+//step 6
+
+function howdy( person, message ) {
+    return person + ' says ' + message;
+}
+
+var message = howdy( 'Janetta Boone', 'hello Caitlyn!' );
+console.log( message );
+
+//6b
+
+var numba = function(string) {
+  if (string.length < 7) {
+    return('What a tiny little word!');
+  } else if (string.length > 7) {
+    return("I'm sorry, but that is too many to count");
+  } else if (string.length === 7) {
+    return('7, what a perfect choice!');
+  }
+}
+
+console.log(numba('7'));
+console.log(numba('seventy'));
+console.log(numba('university'));
+
+//6c
+
+function inception (display, faveMovie) {
+  console.log(faveMovie + ' ' + 'is a fantastic movie.')
+}
+
+inception ('show', 'Age of Adeline');
+function display(faveMovie) {
+};
